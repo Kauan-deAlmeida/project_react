c2cdc48ff2ce97a7eb984531b6849fe4e4aa867d
Kauan-deAlmeida <kauan.almeida@sptech.school>
2025-05-14 16:06:51 -0300

    feat: Usando useRef com objetos simples

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom"
 import { Button } from "../../shared/components/ButtonNavigacao"
+import { useRef } from "react"
 
 export const Dashboard =() => {
+
+    const counterRef = useRef({ counter: 0});
+
     return ( 
         <div>
             <p>Dashboard</p>
+
+            <p>Contador: {counterRef.current.counter}</p>
+            
+            <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Somar</button>
+
             <Link to="/entrar">Login</Link>
             <Button
                 texto="Entrar"
