398d75a0e9fe5814add90951a39b5c7af0af8f70
Kauan-deAlmeida <kauan.almeida@sptech.school>
2025-05-14 09:54:28 -0300

    feat: Add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,14 +1,39 @@
-import { Button } from "../../shared/components/ButtonNavigacao";
+import { useState } from "react";
 
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email)
+    }
 
     return (
         <div>
-            Login
-            <Button
-                texto="Página inicial"
-                pagina="/pagina-inicial"
-            />
+            <form>
+
+                <label>
+                    <span>Email</span>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input
+                        type="password"
+                        value={senha}
+                        onChange={e => setSenha(e.target.value)}
+                    />
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
         </div>
     );
 }
